f45739b2c90d4d628fbd6957c71b13519cbd5028
Björn Hagström <orgin.org@gmail.com>
2012-11-21 13:19:01 +0100

    Removed warning

--- src/biz/orgin/minecraft/hothgenerator/StructureGrowManager.java
+++ src/biz/orgin/minecraft/hothgenerator/StructureGrowManager.java
@@ -1,7 +1,6 @@
 package biz.orgin.minecraft.hothgenerator;
 
 import org.bukkit.World;
-import org.bukkit.block.Block;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
