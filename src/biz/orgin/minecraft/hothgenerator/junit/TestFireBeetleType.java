f0b83944b9ad7accfcebc6c5f46e8126be2270c5
My Name <orgin.org@gmail.com>
2015-03-09 10:53:16 +0100

    Added test for firebeetletype

---
+++ src/biz/orgin/minecraft/hothgenerator/junit/TestFireBeetleType.java
@@ -0,0 +1,20 @@
+package biz.orgin.minecraft.hothgenerator.junit;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import biz.orgin.minecraft.hothgenerator.FireBeetle;
+import biz.orgin.minecraft.hothgenerator.FireBeetle.FireBeetleType;
+
+public class TestFireBeetleType {
+
+	@Test
+	public void testGetFireBeetleType() {
+		assertTrue(FireBeetle.getFireBeetleType(FireBeetleType.HATCHLING.getName())==FireBeetleType.HATCHLING);
+		assertTrue(FireBeetle.getFireBeetleType(FireBeetleType.PRIME.getName())==FireBeetleType.PRIME);
+		assertTrue(FireBeetle.getFireBeetleType(FireBeetleType.REGULAR.getName())==FireBeetleType.REGULAR);
+		assertTrue(FireBeetle.getFireBeetleType("__fakename__")==null);
+	}
+
+}
